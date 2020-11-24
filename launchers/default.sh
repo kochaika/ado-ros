1f442f5a8a8cfc05169733ee78d3a2413ce71fa2
Konstantin <pro100kot14@gmail.com>
2020-11-24 17:28:20 +0300

    Add circle drive demo

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,7 +13,8 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
+#dt-exec echo "This is an empty launch script. Update it to launch your application."
+dt-exec roslaunch circle_drive circle_drive.launch
 
 
 # ----------------------------------------------------------------------------
